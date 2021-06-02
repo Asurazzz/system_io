0492c6862d1856dc477812ecda4355e1a3854316
develop3 <2513667783@qq.com>
2021-06-02 22:21:52 +0800

    增加客户端代码

---
+++ 02_nio/src/main/java/com/ymj/system/io/netty/MyNetty.java
@@ -0,0 +1,148 @@
+package com.ymj.system.io.netty;
+
+import io.netty.buffer.*;
+import io.netty.channel.*;
+import io.netty.channel.internal.ChannelUtils;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author : yemingjie
+ * @date : 2021/6/1 21:48
+ */
+public class MyNetty {
+
+    @Test
+    public void myByteBuf() {
+        // 超出最大20就会报错
+        //ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(8, 20);
+        //ByteBuf buf = UnpooledByteBufAllocator.DEFAULT.heapBuffer(8, 20);
+        ByteBuf buf = PooledByteBufAllocator.DEFAULT.heapBuffer(8, 20);
+        print(buf);
+
+        buf.writeBytes(new byte[]{1,2,3,4});
+        print(buf);
+        buf.writeBytes(new byte[]{1,2,3,4});
+        print(buf);
+        buf.writeBytes(new byte[]{1,2,3,4});
+        print(buf);
+        buf.writeBytes(new byte[]{1,2,3,4});
+        print(buf);
+        buf.writeBytes(new byte[]{1,2,3,4});
+        print(buf);
+//        buf.writeBytes(new byte[]{1,2,3,4});
+//        print(buf);
+    }
+
+
+    public static void print(ByteBuf buf) {
+        // 是否可读
+        System.out.println("buf.isReadable(): " + buf.isReadable());
+        // buffer数组中从哪可以读
+        System.out.println("buf.readerIndex():  "+ buf.readerIndex());
+        // 可以读多少
+        System.out.println("buf.readableBytes():    " + buf.readableBytes());
+        // 是否可写
+        System.out.println("buf.isWritable()    " + buf.isWritable());
+        // 从哪开始写
+        System.out.println("buf.writerIndex()   " + buf.writerIndex());
+        // 还可以写的字节数，和初始容量相关
+        System.out.println("buf.writableBytes()     " + buf.writableBytes());
+        // 上限
+        System.out.println("buf.capacity()  " + buf.capacity());
+        // 最大上限
+        System.out.println("buf.maxCapacity()   " + buf.maxCapacity());
+        // false 堆内还是   true 堆外
+        System.out.println("buf.isDirect()  " + buf.isDirect());
+        System.out.println("=================================");
+    }
+
+
+    @Test
+    public void loopExecutor() throws Exception {
+        NioEventLoopGroup selector = new NioEventLoopGroup(1);
+        selector.execute(() -> {
+            try {
+                for (;;) {
+                    System.out.println("hello World001");
+                    Thread.sleep(1000);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        selector.execute(() -> {
+            try {
+                for (;;) {
+                    System.out.println("hello World002");
+                    Thread.sleep(1000);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        System.in.read();
+    }
+
+
+    /**
+     * 客户端
+     * @throws Exception
+     */
+    @Test
+    public void clientModel() throws Exception{
+        NioEventLoopGroup thread = new NioEventLoopGroup(1);
+
+        NioSocketChannel client = new NioSocketChannel();
+        thread.register(client);
+
+
+        // 响应式：可以打印出从服务端打出的信息
+        ChannelPipeline pipeline = client.pipeline();
+        pipeline.addLast(new MyInHandler());
+
+        // reactor 异步
+        ChannelFuture connect = client.connect(new InetSocketAddress("192.168.195.132", 9090));
+        ChannelFuture sync = connect.sync();
+
+        ByteBuf byteBuf = Unpooled.copiedBuffer("hello server....".getBytes(StandardCharsets.UTF_8));
+        ChannelFuture send = client.writeAndFlush(byteBuf);
+        send.sync();
+
+        sync.channel().closeFuture().sync();
+        System.out.println("client over...");
+    }
+}
+
+class MyInHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client register......");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client active......");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buffer = (ByteBuf) msg;
+        // 会移动指针
+        //CharSequence charSequence = buffer.readCharSequence(buffer.readableBytes(), CharsetUtil.UTF_8);
+        // 不会移动指针
+        CharSequence charSequence = buffer.getCharSequence(0, buffer.readableBytes(), CharsetUtil.UTF_8);
+        // 打印在本地
+        System.out.println(charSequence);
+        // 给别人打印回去
+        ctx.writeAndFlush(buffer);
+    }
+}
